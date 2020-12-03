843cabc3d34317c958f3d4ee4c9e23406b84be0d
Ismael Barragan <ismael.barraganlopez@salesloft.com>
2020-12-03 17:32:56 -0600

    Optimize code (DRY)

--- days/day03/index.ts
+++ days/day03/index.ts
@@ -2,8 +2,21 @@ import { readInput } from '../../common'
 
 const input = readInput(`${__dirname}/input`).map((row) => row.split(''));
 
-function walk1() {
-  const slope = [3, 1];
+function walk1(): number {
+  const slope: [number, number] = [3, 1];
+
+  return walkTheWalk(slope, input);
+}
+
+function walk2(): number {
+  const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+
+  return slopes.reduce((total: number, slope: [number, number]) => {
+    return total * walkTheWalk(slope, input);
+  }, 1)
+}
+
+function walkTheWalk(slope: [number, number], input: string[][]): number {
   const width = input[0].length;
   const position = [0, 0];
   let treeCount = 0;
@@ -12,31 +25,9 @@ function walk1() {
     treeCount += input[position[0]][position[1] % width] === '#' ? 1 : 0;
     position[1] += slope[0];
     position[0] += slope[1];
-    console.log(treeCount);
   }
 
   return treeCount;
-}
-
-function walk2() {
-  const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
-  const width = input[0].length;
-  let total = 1;
-
-  slopes.forEach((slope) => {
-    const position = [0, 0];
-    let treeCount = 0;
-
-    while (position[0] < input.length) {
-      treeCount += input[position[0]][position[1] % width] === '#' ? 1 : 0;
-      position[1] += slope[0];
-      position[0] += slope[1];
-    }
-
-    total *= treeCount;
-  })
-
-  return total;
 }
 
 process.stdout.write(`Part 1: ${walk1()}\n`);
